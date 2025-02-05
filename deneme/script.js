cb1693fb4e10f931d9aad7c6d664ef7fdba4e424
Seviye <seviyemert99@gmail.com>
2025-02-05 23:38:30 +0300

    gün 3

---
+++ deneme/script.js
@@ -0,0 +1,19 @@
+
+
+
+let sayi = Number(prompt("Lütfen bir sayı giriniz!"));
+let sonuc = true;
+
+for(let i=2 ; i<= Math.floor(sayi/2); i++){
+    if(sayi%i==0){
+        //Asal değildir
+        sonuc=false;
+        console.log("Asal değildir")
+        break;
+    }
+}
+if(sonuc){
+    alert(sayi +" asaldır.");
+}else{
+    alert(sayi +" asal değildir!");
+}
